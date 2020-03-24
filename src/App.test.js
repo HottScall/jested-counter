44a7e3a6468681186c16079f08250f02b8f638f0
Scott <Scotthall1983@hotmail.com>
2020-03-24 14:31:18 +0000

    installed enzyme onto project, imported relevant details into setupTest.js. Wrote a test which shallow renders the App, set a wrapper to find the p tag text and expected that text to be equal to zero

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("App component", () => {
+  it("starts with a count of zero", () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Count: 0");
+  });
 });
